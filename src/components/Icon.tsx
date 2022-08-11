1f4a323b4133ad3704e5e8d2547874c4581933a8
Andres Michel <andresmichelg@gmail.com>
2022-08-11 13:41:58 -0700

    Revert "Remove Image component"
    This reverts commit 9ad219f5491b6fc7d9284c8a08383af0598e9bdc.

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Icon = (props: { src: string; alt: string; onClick: () => void }) => {
   return (
     <div
@@ -5,7 +7,7 @@ const Icon = (props: { src: string; alt: string; onClick: () => void }) => {
       className="shadow-md h-32 lg:h-100 w-32 lg:w-100 rounded-2xl lg:rounded-3xl overflow-hidden"
     >
       <a href={props.src} download>
-        <img height={200} width={200} src={props.src} alt={props.alt} />
+        <Image height={200} width={200} src={props.src} alt={props.alt} />
       </a>
     </div>
   );
